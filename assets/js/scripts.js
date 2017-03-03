def5208e8ddd6b26573757f42974b06083b13973
Nizamuddin Sulieman <kuntau17@gmail.com>
2017-03-04 02:59:52 +0800

    add navbar, still figuring the margin fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
+
   $(".dropdown-button").dropdown({
     hover: true
   });
+
   $(".button-collapse").sideNav();
+
   $(".slider").slider({
     height: 400,
     full_width: true,
     indicators: false
   });
+
+  $(".parallax").parallax();
 })
